dbe33ca0116c646a41fd8a5160cefc16ff905fdd
Aigars Kokins <kokins.aigars@gmail.com>
2024-07-01 17:03:35 +0300

    DEE_2024.07.01._04

--- src/main/_user/landing-page/components/body/hexahedron.vue
+++ src/main/_user/landing-page/components/body/hexahedron.vue
@@ -14,32 +14,38 @@
 
 <template>
 
-    <div class="div_cls_svg_form">
+      <div class="div_container">
 
-      <div class="title-formas T-title-formas">Hexahedron</div>
-      <div class="GPS-formas T-GPS-formas">56°34'18.9"N 23°04'24.4"E</div>
 
-      <svg class="cls_svg_hexahedron"
-           x="0px" y="0px" viewBox="0 0 480 480"
-           style="user-select:none; overflow:hidden;" aria-hidden="true" focusable="false" role="img"
-           xmlns="http://www.w3.org/2000/svg">
-        <g>
-          <line class="st_svg_hex" x1="450" y1="82.17" x2="190.12" y2="7.35"/>
-          <line class="st_svg_hex" x1="450" y1="82.17" x2="450" y2="343.18"/>
-          <line class="st_svg_hex" x1="190.12" y1="268.36" x2="450" y2="343.18"/>
-          <line class="st_svg_hex" x1="190.12" y1="7.35" x2="190.12" y2="268.36"/>
-          <line class="st_svg_hex" x1="299.06" y1="210.99" x2="450" y2="82.17"/>
-          <line class="st_svg_hex" x1="299.06" y1="210.99" x2="299.06" y2="472"/>
-          <line class="st_svg_hex" x1="450" y1="343.18" x2="299.06" y2="472"/>
-          <line class="st_svg_hex" x1="190.12" y1="7.35" x2="39.18" y2="136.17"/>
-          <line class="st_svg_hex" x1="39.18" y1="397.18" x2="190.12" y2="268.36"/>
-          <line class="st_svg_hex" x1="39.18" y1="136.17" x2="299.06" y2="210.99"/>
-          <line class="st_svg_hex" x1="39.18" y1="136.17" x2="39.18" y2="397.18"/>
-          <line class="st_svg_hex" x1="299.06" y1="472" x2="39.18" y2="397.18"/>
-        </g>
-      </svg>
+        <div class="image-container">
+          <svg class="cls_svg_hexahedron"
+               x="0px" y="0px" viewBox="0 0 480 480"
+               style="user-select:none; overflow:hidden;" aria-hidden="true" focusable="false" role="img"
+               xmlns="http://www.w3.org/2000/svg">
+            <g>
+              <line class="st_svg_hex" x1="450" y1="82.17" x2="190.12" y2="7.35"/>
+              <line class="st_svg_hex" x1="450" y1="82.17" x2="450" y2="343.18"/>
+              <line class="st_svg_hex" x1="190.12" y1="268.36" x2="450" y2="343.18"/>
+              <line class="st_svg_hex" x1="190.12" y1="7.35" x2="190.12" y2="268.36"/>
+              <line class="st_svg_hex" x1="299.06" y1="210.99" x2="450" y2="82.17"/>
+              <line class="st_svg_hex" x1="299.06" y1="210.99" x2="299.06" y2="472"/>
+              <line class="st_svg_hex" x1="450" y1="343.18" x2="299.06" y2="472"/>
+              <line class="st_svg_hex" x1="190.12" y1="7.35" x2="39.18" y2="136.17"/>
+              <line class="st_svg_hex" x1="39.18" y1="397.18" x2="190.12" y2="268.36"/>
+              <line class="st_svg_hex" x1="39.18" y1="136.17" x2="299.06" y2="210.99"/>
+              <line class="st_svg_hex" x1="39.18" y1="136.17" x2="39.18" y2="397.18"/>
+              <line class="st_svg_hex" x1="299.06" y1="472" x2="39.18" y2="397.18"/>
+            </g>
+          </svg>
+        </div>
 
-    </div>
+        <div class="text-container">
+          <div class="title-formas T-title-formas">Hexahedron</div>
+          <div class="GPS-formas T-GPS-formas">56°34'18.9"N 23°04'24.4"E</div>
+        </div>
+
+      </div>
+
 
 </template>
 
--- src/main/_user/landing-page/components/body/tetrahedron.vue
+++ src/main/_user/landing-page/components/body/tetrahedron.vue
@@ -14,24 +14,29 @@
 
 <template>
 
-  <div class="div_cls_svg_form">
+  <div class="div_container">
 
-    <div class="title-formas T-title-formas">Tetrahedron</div>
-    <div class="GPS-formas T-GPS-formas">57°00'57.4"N 21°34'51.1"E</div>
+    <div class="image-container">
+      <svg class="cls_svg_tetrahedron"
+           x="0px" y="0px" viewBox="0 0 480 480"
+           style="user-select:none; overflow:hidden;" aria-hidden="true" focusable="false" role="img"
+           xmlns="http://www.w3.org/2000/svg">
+        <g>
+          <line class="st_svg_hex" x1="39.18" y1="269.15" x2="427.54" y2="449.13"/>
+          <line class="st_svg_hex" x1="39.18" y1="269.15" x2="302.24" y2="30.22"/>
+          <line class="st_svg_hex" x1="427.54" y1="449.13" x2="440" y2="105.43"/>
+          <line class="st_svg_hex" x1="302.24" y1="30.22" x2="440" y2="105.43"/>
+          <line class="st_svg_hex" x1="427.54" y1="449.13" x2="302.24" y2="30.22"/>
+          <line class="st_svg_hex" x1="440" y1="105.43" x2="39.18" y2="269.15"/>
+        </g>
+      </svg>
 
-    <svg class="cls_svg_tetrahedron"
-         x="0px" y="0px" viewBox="0 0 480 480"
-         style="user-select:none; overflow:hidden;" aria-hidden="true" focusable="false" role="img"
-         xmlns="http://www.w3.org/2000/svg">
-      <g>
-        <line class="st_svg_hex" x1="39.18" y1="269.15" x2="427.54" y2="449.13"/>
-        <line class="st_svg_hex" x1="39.18" y1="269.15" x2="302.24" y2="30.22"/>
-        <line class="st_svg_hex" x1="427.54" y1="449.13" x2="440" y2="105.43"/>
-        <line class="st_svg_hex" x1="302.24" y1="30.22" x2="440" y2="105.43"/>
-        <line class="st_svg_hex" x1="427.54" y1="449.13" x2="302.24" y2="30.22"/>
-        <line class="st_svg_hex" x1="440" y1="105.43" x2="39.18" y2="269.15"/>
-      </g>
-    </svg>
+    </div>
+
+    <div class="text-container">
+      <div class="title-formas T-title-formas">Tetrahedron</div>
+      <div class="GPS-formas T-GPS-formas">57°00'57.4"N 21°34'51.1"E</div>
+    </div>
 
   </div>
 
