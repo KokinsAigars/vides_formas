46bdfb564a024b088a51ad66b5f5694c431c2fa5
KokinsAigars <kokins.aigars@gmail.com>
2024-09-05 20:02:13 +0300

    RIX_2024.09.05._01

--- src/main/components/home/home.vue
+++ src/main/components/home/home.vue
@@ -18,7 +18,12 @@
 
 <template>
   <div class="cnn">
-
+    <div class="h">
+      <p class="t-hp">The Top 8 Factors</p>
+    </div>
+    <div class="b">
+      <p class="bp t-bp">These are the top 8 SEO ranking factors. There are 200+ other factors that Google takes into account, but these are the ones that you should focus on first:</p>
+    </div>
   </div>
 </template>
 
@@ -29,6 +34,28 @@
   height: 100vh;
   background: var(--c-home-bg);
   overflow:hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.h{
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 10px;
+}
+.b{
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: auto;
+
+  .bp {
+    margin: auto;
+    width: 80%;
+    text-align: left;
+  }
 }
 
 </style>
